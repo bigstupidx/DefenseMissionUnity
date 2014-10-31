473a9f736fc0f5026313584295359e594baddac5
Sergey Krivich <dvdkong2007@gmail.com>
2014-10-31 21:50:22 +0200

    Turned on google analytics

--- Assets/Plugins/GoogleAnalytics/GoogleAnalytics.cs
+++ Assets/Plugins/GoogleAnalytics/GoogleAnalytics.cs
@@ -37,31 +37,41 @@ public class GoogleAnalytics : MonoBehaviour {
 
 	public void LogScreen(string title)
 	{
-		
-		title = WWW.EscapeURL(title);
-		
-		var url = "http://www.google-analytics.com/collect?v=1&ul=en-us&t=appview&sr="+screenRes+"&an="+WWW.EscapeURL(appName)+"&a=448166238&tid="+propertyID+"&aid="+bundleID+"&cid="+WWW.EscapeURL(clientID)+"&_u=.sB&av="+appVersion+"&_v=ma1b3&cd="+title+"&qt=2500&z=185";
-		
-		WWW request = new WWW(url);
-		
-		/*if(request.error == null)
-		{
-			if (request.responseHeaders.ContainsKey("STATUS"))
-			{
-				if (request.responseHeaders["STATUS"] == "HTTP/1.1 200 OK")	
-				{
-					Debug.Log ("GA Success");
-				}else{
-					Debug.LogWarning(request.responseHeaders["STATUS"]);	
-				}
-			}else{
-				Debug.LogWarning("Event failed to send to Google");	
-			}
-		}else{
-			Debug.LogWarning(request.error.ToString());	
-		}*/
-		
+	    StartCoroutine(LogRequest(title));
 	}
-	
-	
+
+    private IEnumerator LogRequest(string title)
+    {
+        title = WWW.EscapeURL(title);
+
+        var url = "http://www.google-analytics.com/collect?v=1&ul=en-us&t=appview&sr=" + screenRes + "&an=" +
+                  WWW.EscapeURL(appName) + "&a=448166238&tid=" + propertyID + "&aid=" + bundleID + "&cid=" +
+                  WWW.EscapeURL(clientID) + "&_u=.sB&av=" + appVersion + "&_v=ma1b3&cd=" + title + "&qt=2500&z=185";
+
+        WWW request = new WWW(url);
+        yield return request;
+
+        if (request.error == null)
+        {
+            if (request.responseHeaders.ContainsKey("STATUS"))
+            {
+                if (request.responseHeaders["STATUS"] == "HTTP/1.1 200 OK")
+                {
+                    //Debug.Log("GA Success");
+                }
+                else
+                {
+                    //Debug.LogWarning(request.responseHeaders["STATUS"]);
+                }
+            }
+            else
+            {
+               // Debug.LogWarning("Event failed to send to Google");
+            }
+        }
+        else
+        {
+           // Debug.LogWarning(request.error.ToString());
+        }
+    }
 }
