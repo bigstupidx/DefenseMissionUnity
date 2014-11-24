8b2367e7d6cd9158fda849c61505b515b3e48455
Sergey Krivich <sergeyk@reddotsquare.com>
2014-11-25 00:15:33 +0200

    Base scene

---
+++ Assets/Editor/ChangeObjectsTag.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class ChangeObjectsTag : MonoBehaviour
+{
+    private const string cTargetTag = "Death";
+
+    [MenuItem ("GameObject/SetTag")]
+    private static void ChangeTag()
+    {
+        GameObject selection = Selection.gameObjects.FirstOrDefault();
+        SetTag(selection);
+    }
+
+    private static void SetTag(GameObject gameObject)
+    {
+        gameObject.tag = cTargetTag;
+
+        foreach (Transform t in gameObject.transform)
+        {
+            SetTag(t.gameObject);
+        }
+    }
+
+}
