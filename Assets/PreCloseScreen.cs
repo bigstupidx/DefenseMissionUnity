7c878e8cdbcde1adccf4e213f519058b416f10f5
Sergey Krivich <sergeyk@reddotsquare.com>
2014-11-30 13:15:04 +0200

    Fix city atlas, fix preclose screen closing

--- Assets/PreCloseScreen.cs
+++ Assets/PreCloseScreen.cs
@@ -5,17 +5,23 @@ using System.Collections.Generic;
 public class PreCloseScreen : MonoBehaviour, IEventSubscriber
 {
 	public List<PreCloseScreenImageElement> Images;
+    public PreCloseScreen Instance;
 	
 	private bool _placedImages;
 
 	void Start()
 	{
+	    Instance = this;
+
 		if(PrecloseScreenIAS.Instance.preReady)
 		{
 			PlaceImages();
 		}
 
 		EventController.Instance.Subscribe("LoadedIAS", this);
+        EventController.Instance.Subscribe("OnPrecloseAdClick", this);
+        EventController.Instance.Subscribe("OnClosePreCloseScreen", this);
+        EventController.Instance.Subscribe("RateGame", this);
 	}
 
 	void Update()
@@ -44,43 +50,54 @@ public class PreCloseScreen : MonoBehaviour, IEventSubscriber
 		}
 		else if(EventName == "OnPrecloseAdClick")
 		{
+            Debug.Log("Show ad");
 			ShowAdd();
 		}
-		else if(EventName == "OnClosePreCloseScreen")
+        else if (EventName == "OnClosePreCloseScreen")
 		{
+            Debug.Log("Close preclose screen");
 			Close ();
 		}
 	}
 
-	void ShowAdd ()
-	{
-		throw new System.NotImplementedException ();
-	}
-
-	private void PlaceImages()
-	{
-		_placedImages = true;
-		foreach (var texture in PrecloseScreenIAS.Instance.preBannerTextures) 
-		{
-			var imageElement = Images.Find(p => !p.Placed);
-			if(imageElement == null)
-			{
-				break;
-			}
-			imageElement.Image.material.mainTexture = texture;
-			imageElement.Placed = true;
-		}
-	}
-
-	void Close ()
-	{
-		foreach (Transform t in transform) 
-		{
-			t.GetComponent<MeshRenderer> ().enabled = false;
-			t.GetComponent<BoxCollider> ().enabled = false;
-		}
-		transform.GetComponent<MeshRenderer> ().enabled = false;
-		transform.GetComponent<BoxCollider> ().enabled = false;
-	}
 	#endregion
+
+
+    void ShowAdd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void PlaceImages()
+    {
+        _placedImages = true;
+        foreach (var texture in PrecloseScreenIAS.Instance.preBannerTextures)
+        {
+            var imageElement = Images.Find(p => !p.Placed);
+            if (imageElement == null)
+            {
+                break;
+            }
+            imageElement.Image.material.mainTexture = texture;
+            imageElement.Placed = true;
+        }
+    }
+
+    void Close()
+    {
+        gameObject.SetActive(false);
+//        foreach (Transform t in transform)
+//        {
+//            if (t.GetComponent<MeshRenderer>())
+//            {
+//                t.GetComponent<MeshRenderer>().enabled = false;
+//            }
+//            if (t.GetComponent<BoxCollider>())
+//            {
+//                t.GetComponent<BoxCollider>().enabled = false;
+//            }
+//        }
+//        transform.GetComponent<MeshRenderer>().enabled = false;
+//        transform.GetComponent<BoxCollider>().enabled = false;
+    }
 }
