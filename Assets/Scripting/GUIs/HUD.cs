1e47f2f0988863e90b83589dd32919a6517eaf9d
Sergey Krivich <sergeyk@reddotsquare.com>
2014-12-22 22:34:14 +0200

    Fix hud decimal points

--- Assets/Scripting/GUIs/HUD.cs
+++ Assets/Scripting/GUIs/HUD.cs
@@ -19,7 +19,7 @@ public class HUD : MonoBehaviour
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-	    Speed.text = AirplaneController.Instance.CurrentSpeed.ToString();
+	    Speed.text = AirplaneController.Instance.CurrentSpeed.ToString("0.0");
 	    Height.text = ((int)(AirplaneController.Instance.transform.position.y*1.28084f)).ToString();
 
         var rotation = Center.rotation.eulerAngles;
