eecd7b8f208a973709e20332a8c3eadd1fde56dc
Sergey Krivich <sergeyk@reddotsquare.com>
2015-01-22 00:06:00 +0200

    temp changes

--- Assets/Scripting/GUIs/HUD.cs
+++ Assets/Scripting/GUIs/HUD.cs
@@ -12,10 +12,17 @@ public class HUD : MonoBehaviour
 
     public Radar Radar;
 
+    public float HeightSizeDisplay = 10f;
+    public Transform NumberHeightRoot;
+    public GameObject NumberPointPrefab;
+
+    private const float NumberOffset = 100f;
+    private const float NumberOffsetHeight = 2f;
+
 	// Use this for initialization
-	void Start () 
-    {
-	
+	void Start ()
+	{
+	    GenerateHeightIndicator();
 	}
 	
 	// Update is called once per frame
@@ -48,5 +55,28 @@ public class HUD : MonoBehaviour
 	    pos.y = Mathf.Lerp(Top.localPosition.y, Bottom.localPosition.y, 1f - ceof)*1f;
 
         Center.localPosition = pos;
+
+
+
+	    UpdateHeightIndicator();
 	}
+
+    private void GenerateHeightIndicator()
+    {
+        for (int i = -2; i < 50; i++)
+        {
+            GameObject numberPoint = GameObject.Instantiate(NumberPointPrefab) as GameObject;
+            numberPoint.transform.parent = NumberHeightRoot;
+            numberPoint.transform.localPosition = new Vector3(0, i * NumberOffsetHeight, 0);
+        }
+    }
+
+    private void UpdateHeightIndicator()
+    {
+        float height = Mathf.Clamp(AirplaneController.Instance.transform.position.y, 0, 1006660f);
+
+
+        NumberHeightRoot.transform.localPosition = new Vector3(0, (height / NumberOffset) * NumberOffsetHeight, 0);
+
+    }
 }
