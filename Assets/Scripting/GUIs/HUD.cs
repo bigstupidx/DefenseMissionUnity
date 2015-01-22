637b6622e7b9b72d5b006003b6326a0277f6fe49
Sergey Krivich <sergeyk@reddotsquare.com>
2015-01-22 14:05:50 +0200

    Simple height hud

--- Assets/Scripting/GUIs/HUD.cs
+++ Assets/Scripting/GUIs/HUD.cs
@@ -13,11 +13,15 @@ public class HUD : MonoBehaviour
     public Radar Radar;
 
     public float HeightSizeDisplay = 10f;
+
+    public float HeightYOffset;
+
     public Transform NumberHeightRoot;
     public GameObject NumberPointPrefab;
 
     private const float NumberOffset = 100f;
     private const float NumberOffsetHeight = 2f;
+
 
 	// Use this for initialization
 	void Start ()
@@ -63,20 +67,22 @@ public class HUD : MonoBehaviour
 
     private void GenerateHeightIndicator()
     {
-        for (int i = -2; i < 50; i++)
+        for (int i = -10; i < 100; i++)
         {
             GameObject numberPoint = GameObject.Instantiate(NumberPointPrefab) as GameObject;
             numberPoint.transform.parent = NumberHeightRoot;
             numberPoint.transform.localPosition = new Vector3(0, i * NumberOffsetHeight, 0);
+            numberPoint.transform.localEulerAngles = new Vector3(0, 0, 0);
+            numberPoint.transform.localScale = new Vector3(1, 1, 1);
+            numberPoint.SetActive(true);
+
+            numberPoint.GetComponentInChildren<TextMesh>().text = (i*NumberOffset).ToString();
         }
     }
 
     private void UpdateHeightIndicator()
     {
         float height = Mathf.Clamp(AirplaneController.Instance.transform.position.y, 0, 1006660f);
-
-
-        NumberHeightRoot.transform.localPosition = new Vector3(0, (height / NumberOffset) * NumberOffsetHeight, 0);
-
+        NumberHeightRoot.transform.localPosition = new Vector3(NumberHeightRoot.transform.localPosition.x, -(height / NumberOffset) * NumberOffsetHeight + HeightYOffset, NumberHeightRoot.transform.localPosition.z);
     }
 }
