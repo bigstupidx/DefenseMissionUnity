d93b7d025a42f31ea364070c2547f23794da9774
Sergey Krivich <dvdkong2007@gmail.com>
2015-03-16 23:21:11 +0200

    Height text throtle fix

---
+++ Assets/HelpTextHeight.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpTextHeight : MonoBehaviour {
+
+
+    private TextMesh mText;
+
+    void Start()
+    {
+        mText = GetComponent<TextMesh>();
+        mText.text = "";
+    }
+	
+	// Update is called once per frame
+    private void Update()
+    {
+        if (AirplaneController.Instance.State == AirplaneStates.Fly &&
+            MissionController.Instance.CurrentState is FollowingWaypoints)
+        {
+            if (BaseLevel.Instance.Height != 0)
+            {
+                mText.text = "Keep your height below " + BaseLevel.Instance.Height + "ft";
+            }
+            else
+            {
+                SetDefault();
+            }
+        }
+        else
+        {
+            SetDefault();
+        }
+    }
+
+    private void SetDefault()
+    {
+        if (mText.text != "")
+            mText.text = "";
+    }
+}
