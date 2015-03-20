e584bba4282ab542c48797cdba0306429d190b7e
Sergey Krivich <dvdkong2007@gmail.com>
2015-03-20 21:39:57 +0200

    Height Animation

--- Assets/HelpTextHeight.cs
+++ Assets/HelpTextHeight.cs
@@ -5,7 +5,7 @@ public class HelpTextHeight : MonoBehaviour {
 
 
     private TextMesh mText;
-
+    private bool _startedAnimation;
     void Start()
     {
         mText = GetComponent<TextMesh>();
@@ -22,6 +22,7 @@ public class HelpTextHeight : MonoBehaviour {
             if (BaseLevel.Instance.Height != 0)
             {
                 mText.text = "Keep your height below " + BaseLevel.Instance.Height + "ft";
+                StartCoroutine(Animate());
             }
             else
             {
@@ -31,6 +32,31 @@ public class HelpTextHeight : MonoBehaviour {
         else
         {
             SetDefault();
+        }
+    }
+
+    private IEnumerator Animate()
+    {
+        if (!_startedAnimation)
+        {
+
+            _startedAnimation = true;
+            yield return new WaitForSeconds(3.0f);
+
+            Vector3 targetPos = new Vector3(-7.4f, 4.0f, 3.7f);
+            const float targetScale = 0.09f;
+
+            for (float t = 0; t < 3.0; t+=Time.deltaTime)
+            {
+                transform.localPosition = Vector3.Lerp(transform.localPosition, targetPos, Time.deltaTime*3);
+
+                float scale = transform.localScale.x;
+                scale = Mathf.Lerp(scale, targetScale, Time.deltaTime);
+
+                transform.localScale = new Vector3(scale, scale,1f);
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 
